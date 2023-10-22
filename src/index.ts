e04c52cc738ee0fdf9b9bba007eeeaee9b1c3d5b
Tix <tix.personal@gmail.com>
2023-10-22 07:41:24 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Client, Guild } from "discord.js";
+import { config } from "./config";
+import { commands } from "./commands";
+import { deployCommands } from "./deploy-commands";
+
+const client = new Client({
+    intents: ["Guilds", "GuildMessages", "DirectMessages"],
+});
+
+client.once("ready", () => {
+    console.log("Discord bot is ready! 🤖");
+});
+
+async function onGuild(guild: Guild) {
+    deployCommands({ guildId: guild.id });
+}
+client.on("guildCreate", onGuild);
+client.on("guildAvailable", onGuild);
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    const { commandName } = interaction;
+    if (commands[commandName as keyof typeof commands]) {
+        commands[commandName as keyof typeof commands].execute(interaction);
+    }
+});
+
+client.login(config.DISCORD_TOKEN);
